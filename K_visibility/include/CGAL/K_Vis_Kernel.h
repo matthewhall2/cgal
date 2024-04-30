10f0c572c9b04b94f1dc550dc24447a5a5f18890
matthewhall2 <matthew.hall3@outlook.com>
2024-04-30 02:55:12 -0400

    more point attributes

--- K_visibility/include/CGAL/K_Vis_Kernel.h
+++ K_visibility/include/CGAL/K_Vis_Kernel.h
@@ -14,7 +14,7 @@
 // K_ is the new kernel, and K_Base is the old kernel
 template < typename K_, typename K_Base >
 class K_Vis_Base
-  : public K_Base::template Base<K_>::Type
+    : public K_Base::template Base<K_>::Type
 {
   typedef typename K_Base::template Base<K_>::Type   OldK;
 public:
@@ -22,7 +22,7 @@ public:
   typedef typename K_Vis_point_2<OldK>                         Point_2;
   typedef typename K_VisSegment_2<Kernel>               Segment_2;
   typedef typename K_VisConstruct_point_2<Kernel, OldK>       Construct_point_2;
-  typedef typename const K_Base::FT*                     Cartesian_const_iterator_2;
+  typedef typename const K_Base::FT* Cartesian_const_iterator_2;
   typedef typename K_VisConstruct_coord_iterator<OldK>        Construct_cartesian_const_iterator_2;
  typedef typename K_VisConstruct_bbox_2<typename OldK::Construct_bbox_2, K_Base>
                                             Construct_bbox_2;
@@ -31,24 +31,30 @@ public:
 
   Construct_point_2
   construct_point_2_object() const
-  { return Construct_point_2(); }
-
-  Construct_bbox_2
-  construct_bbox_2_object() const
-  { return Construct_bbox_2(); }
-
-  Construct_cartesian_const_iterator_2
-  construct_cartesian_const_iterator_2_object() const
-  { return Construct_cartesian_const_iterator_2(); }
-
-  Construct_segment_2
-      construct_segment_2_object() const
   {
-      return Construct_segment_2();
-  }
+ return Construct_point_2();
+}
 
-  template < typename Kernel2 >
-  struct Base { typedef K_Vis_Base<Kernel2, K_Base>  Type; };
+Construct_bbox_2
+construct_bbox_2_object() const
+{
+return Construct_bbox_2();
+}
+
+Construct_cartesian_const_iterator_2
+construct_cartesian_const_iterator_2_object() const
+{
+return Construct_cartesian_const_iterator_2();
+}
+
+Construct_segment_2
+    construct_segment_2_object() const
+{
+    return Construct_segment_2();
+}
+
+template < typename Kernel2 >
+struct Base { typedef K_Vis_Base<Kernel2, K_Base>  Type; };
 };
 
 
@@ -77,9 +83,9 @@ public:
 //
 template < typename FT_ >
 struct K_Vis_Kernel
-  : public CGAL::Type_equality_wrapper<
-                K_Vis_Base<K_Vis_Kernel<FT_>, CGAL::Cartesian<FT_> >,
-                K_Vis_Kernel<FT_> >
+    : public CGAL::Type_equality_wrapper<
+    K_Vis_Base<K_Vis_Kernel<FT_>, CGAL::Cartesian<FT_> >,
+    K_Vis_Kernel<FT_> >
 {};
 //
 //namespace CGAL {
@@ -118,6 +124,9 @@ public:
         Point_2 r = transform(p);
         r.id() = p.id();
         r.isArtificial() = p.isArtificial();
+        r.isTip() = p.isTip();
+        r.isReflex() = p.isReflex();
+        r.baseAwayFromQP() = p.baseAwayFromQP();
         return r;
     }
 };
--- K_visibility/include/CGAL/K_Vis_point_2.h
+++ K_visibility/include/CGAL/K_Vis_point_2.h
@@ -8,64 +8,72 @@
 template<typename K_>
 class K_Vis_point_2 : public K_::Point_2 {
 private:
-  
-  bool artificial;
-  int id_;
-  bool isTip_;
-  typename K_::FT testx;
-  typename K_::FT testy;
+
+    bool artificial;
+    int id_;
+    bool isReflex_;
+    bool isTip_;
+    bool baseAwayFromQP_;
+    typename K_::FT testx;
+    typename K_::FT testy;
 public:
 
-  K_Vis_point_2(): K_::Point_2(), artificial(false), id_(-1), isTip_(false)
-  {
-     
-  }
+    K_Vis_point_2() : K_::Point_2(), artificial(false), id_(-1), isTip_(false)
+    {
 
-  
-  K_Vis_point_2(const typename K_::FT x, const typename K_::FT y, bool artificial = false, int id=-1) 
-    : K_::Point_2(x, y), testx(x), testy(y), artificial(artificial), id_(id), isTip_(false)
-  {
-     
-  }
-
-  K_Vis_point_2(const typename K_::FT x, const typename K_::FT y, const typename K_::FT w,  bool artificial = false, int id = -1)
-      : K_::Point_2(x, y, w), testx(x), testy(y), artificial(artificial), id_(id)
-  {
-
-  }
-
-  std::string toString() {
-      std::string s = "";
-      std::ostringstream  o;
-   o <<  "[(" << this->hx() << " " << this->hy() << " " << this->hw() << ")" << " id: " << this->id() << " artificial: " << (this->isArtificial() ? "true" : "false") << "]";
-   return o.str();
-  }
-
-  bool isArtificial() const { return artificial; }
-
-  bool& isArtificial() { return artificial; }
-
-  int id() const { return id_; }
-
-  int& id() { return id_; }
-
-  bool isTip() const { return isTip_; }
-  bool& isTip() { return isTip_; }
-
-  void setId(int id) {
-      id_ = id;
-  }
+    }
 
 
-  bool operator==(const K_Vis_point_2 &p) const
-  {
-      return p.x() == this->x() && p.y() == this->y();
-  }
+    K_Vis_point_2(const typename K_::FT x, const typename K_::FT y, bool artificial = false, int id = -1)
+        : K_::Point_2(x, y), artificial(artificial), id_(id), isTip_(false), isReflex_(false), baseAwayFromQP_(false)
+    {
 
-  bool operator!=(const K_Vis_point_2 &p) const
-  {
-      return !(*this == p);
-  }
+    }
+
+    K_Vis_point_2(const typename K_::FT x, const typename K_::FT y, const typename K_::FT w, bool artificial = false, int id = -1)
+        : K_::Point_2(x, y, w), artificial(artificial), id_(id), isTip_(false), isReflex_(false), baseAwayFromQP_(false)
+    {
+
+    }
+
+    std::string toString() {
+        std::string s = "";
+        std::ostringstream  o;
+        o << "[(" << this->hx() << " " << this->hy() << " " << this->hw() << ")" << " id: " << this->id() << " artificial: " << (this->isArtificial() ? "true" : "false") << "]";
+        return o.str();
+    }
+
+    bool isArtificial() const { return artificial; }
+
+    bool& isArtificial() { return artificial; }
+
+    int id() const { return id_; }
+
+    int& id() { return id_; }
+
+    bool isTip() const { return isTip_; }
+    bool& isTip() { return isTip_; }
+
+    bool isReflex() const { return isReflex_; }
+    bool& isReflex() { return isReflex_; }
+
+    bool baseAwayFromQP() const { return baseAwayFromQP_; }
+    bool& baseAwayFromQP() { return baseAwayFromQP_; }
+
+    void setId(int id) {
+        id_ = id;
+    }
+
+
+    bool operator==(const K_Vis_point_2& p) const
+    {
+        return p.x() == this->x() && p.y() == this->y();
+    }
+
+    bool operator!=(const K_Vis_point_2& p) const
+    {
+        return !(*this == p);
+    }
 };
 
 
